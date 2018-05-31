3e683ec6d6a3b490a44f3620a07c3bd053b6b267
b3l7 <wilsondaniel383@gmail.com>
2018-05-31 12:07:26 -0400

    added enemy movement

--- src/sprites/enemy.js
+++ src/sprites/enemy.js
@@ -9,6 +9,9 @@ export default class Enemy extends Phaser.GameObjects.Sprite {
     this.health = 4;
     this.alive = true;
     this.damaged = false;
+    this.canDecide = true;
+    this.moveX = 'none';
+    this.moveY = 'none';
     this.deathSound = this.scene.sound.add('enemyDeathSFX');
     this.deathSound.setVolume(.2);
     this.scene.add.existing(this);
@@ -17,6 +20,35 @@ export default class Enemy extends Phaser.GameObjects.Sprite {
   update(time, delta) 
   {
     if (this.alive) {
+      if (!this.damaged) {
+
+        //decide where to move
+        if (this.canDecide) {
+          this.canDecide = false;
+          this.scene.time.addEvent({ delay: 500, callback: this.resetDecide, callbackScope: this });
+          let decisionX = Phaser.Math.RND.integerInRange(1, 4);
+          if (decisionX === 1 || decisionX === 2) {
+            this.moveX = 'none';
+          } else if (decisionX === 3) {
+            this.moveX = 'up';
+          } else if (decisionX === 4) {
+            this.moveX = 'down';
+          }
+          let decisionY = Phaser.Math.RND.integerInRange(1, 4);
+          if (decisionY === 1 || decisionY === 2) {
+            this.moveY = 'none';
+          } else if (decisionY === 3) {
+            this.moveY = 'left';
+          } else if (decisionY === 4) {
+            this.moveY = 'right';
+          }
+        }
+
+        
+        this.movement();  //call the movement method
+      }
+
+
       if (this.health <= 0) {
         this.deathSound.play();
         this.alive = false;
@@ -26,13 +58,37 @@ export default class Enemy extends Phaser.GameObjects.Sprite {
     }
   }
 
+  movement()
+  {
+    if (this.moveX === 'none') {
+      this.body.setVelocityX(0);
+    } else if (this.moveX === 'up') {
+      this.body.setVelocityX(-32);
+    } else if (this.moveX === 'down') {
+      this.body.setVelocityX(32);
+    }
+
+    if (this.moveY === 'none') {
+      this.body.setVelocityY(0);
+    } else if (this.moveY === 'left') {
+      this.body.setVelocityY(-32);
+    } else if (this.moveY === 'right') {
+      this.body.setVelocityY(32);
+    }
+
+  }
+
+  resetDecide() {
+    this.canDecide = true;
+  }
+
   damage(ammount) 
   {
     if (!this.damaged) {
       this.health -= ammount;
       this.damaged = true;
       this.setTint(0x8e2f15);
-      this.scene.time.addEvent({ delay: 500, callback: this.normalize, callbackScope: this });
+      this.scene.time.addEvent({ delay: 1000, callback: this.normalize, callbackScope: this });
     }
   }
 
